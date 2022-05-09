5b66e22f9c640a8438d2e2ba76663ff40a871ad7
rifkyachong <rifkymarcelio@gmail.com>
2022-05-09 18:14:44 +0700

    create customAnimatedText

---
+++ src/CustomAnimatedText.js
@@ -0,0 +1,76 @@
+import "./CustomAnimatedText.css";
+import React from "react";
+
+export default function CustomAnimatedMultilineText({ children }) {
+  if (!Array.isArray(children)) {
+    return (
+      <CustomAnimatedSinglelineText>{children}</CustomAnimatedSinglelineText>
+    );
+  }
+
+  let indexIterator = 0;
+  let nextIndexIterator = 0;
+  const firstLetterIndexes = children.map((child) => {
+    indexIterator = nextIndexIterator;
+    if (typeof child === "string") {
+      const textContent = child;
+      nextIndexIterator = indexIterator + textContent.length;
+      return indexIterator;
+    }
+
+    const {
+      props: { children: textContent },
+    } = child;
+
+    nextIndexIterator = indexIterator + textContent.length;
+    return indexIterator;
+  });
+
+  return children.map((child, childIndex) => (
+    <CustomAnimatedSinglelineText
+      firstLetterIndex={firstLetterIndexes[childIndex]}
+    >
+      {child}
+    </CustomAnimatedSinglelineText>
+  ));
+}
+
+function CustomAnimatedSinglelineText({ children, firstLetterIndex = 0 }) {
+  if (typeof children === "string") {
+    const textContent = children;
+    return textContent.split("").map((letter, letterIndex) =>
+      /\s+/.test(letter) ? (
+        <span> </span>
+      ) : (
+        <span
+          className="animated-letter"
+          style={{ ["--animation-index"]: firstLetterIndex + letterIndex }}
+        >
+          {letter}
+        </span>
+      )
+    );
+  }
+
+  const {
+    type,
+    props: { children: textContent },
+  } = children;
+
+  return React.createElement(
+    type,
+    {},
+    ...textContent.split("").map((letter, letterIndex) =>
+      /\s+/.test(letter) ? (
+        <span> </span>
+      ) : (
+        <span
+          className="animated-letter"
+          style={{ ["--animation-index"]: firstLetterIndex + letterIndex }}
+        >
+          {letter}
+        </span>
+      )
+    )
+  );
+}
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CustomAnimatedMultilineText from "./CustomAnimatedText";
+import "./Home.scss";
+
+export default function Home() {
+  return (
+    <div id="home-page-container">
+      <CustomAnimatedMultilineText>
+        <h1>Hi! I'm Rifky</h1>
+        <h2>Web Developer</h2>
+      </CustomAnimatedMultilineText>
+    </div>
+  );
+}
